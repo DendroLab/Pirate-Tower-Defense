5734a737dea3941f4db5f6b95926341fb62b0822
avprils <mail.aprilianks@gmail.com>
2021-07-17 18:49:46 +0700

    menu rev

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("PilihMenu", LoadSceneMode.Single);
+    }
+
+    public void pilihLevel1()
+    {
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+    }
+
+    public void pilihLevel2()
+    {
+        SceneManager.LoadScene("Level 2", LoadSceneMode.Single);
+    }
+
+    public void pilihLevel3()
+    {
+        SceneManager.LoadScene("Level 3", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
